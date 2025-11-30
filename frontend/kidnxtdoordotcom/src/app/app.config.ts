b9f1dc53c906bca18c553c338a42866e5678e94b
Keon Marcus <98052746+KeonM@users.noreply.github.com>
2025-11-29 19:35:23 -0500

    backend set up but not connected

--- frontend/kidnxtdoordotcom/src/app/app.config.ts
+++ frontend/kidnxtdoordotcom/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
+  ],
 };
---
+++ frontend/kidnxtdoordotcom/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAj_zlEebN7FFfTRxo8D7-tZNOd2mo9kA8',
+    authDomain: 'kidnxtdoor-dotcom.firebaseapp.com',
+    projectId: 'kidnxtdoor-dotcom',
+    storageBucket: 'kidnxtdoor-dotcom.firebasestorage.app',
+    messagingSenderId: '406808938722',
+    appId: '1:406808938722:web:f901a0d4af3dcadb5cf6bb',
+    measurementId: 'G-77XJKW4V2K',
+  },
+};
